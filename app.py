da5412eb58389a84de5355782cad11b64ac45a45
colin <asdf@gmail.com>
2021-04-14 13:07:04 -0400

    ui changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, request, redirect
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time, json, random, requests, base64
 from models.choice_interface import ChoiceInterface
 
